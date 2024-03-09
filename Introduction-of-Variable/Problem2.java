557fe0739655940531f19822b2fa252abee18782
~Rahul <rborage97@gmail.com>
2024-03-09 17:16:25 +0530

    js file from 1st location

---
+++ Introduction-of-Variable/Problem2.java
@@ -0,0 +1,13 @@
+/******************************************************************************
+Proble 2
+*******************************************************************************/
+public class Problem2
+{
+	public static void main(String[] args) {
+		
+		int a=10;
+		System.out.println(a);
+		 a=20;
+	    System.out.println(a);
+	}
+}
---
+++ Introduction-of-Variable/Problem4.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+Proble 4
+*******************************************************************************/
+public class Problem4
+{
+	public static void main(String[] args) {
+		
+    String Name="";
+    int age=0;
+    
+    Name="Rahul";
+    age=24;
+    
+    System.out.println("Name: "+Name +", "+"age: "+age);
+
+	
+	}
+}
